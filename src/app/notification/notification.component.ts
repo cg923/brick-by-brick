658118281fdf10cbc5dee538d41966b6f09d1c06
cg923 <coreygselover@gmail.com>
2017-09-13 13:42:38 -0600

    Adds css animation to notification

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -9,6 +9,9 @@ import { EmailService } from '../email-service.service';
 export class NotificationComponent implements OnInit {
 
   email;
+  appearing = false;
+  visible = false;
+  disappearing = false;
 
   receiveEmail(email) {
   	this.email = email;
@@ -24,6 +27,25 @@ export class NotificationComponent implements OnInit {
         this.email.subject = this.email.subject.substr(0, 30) + '...';
 
         // CSS animation variables.
+        this.appearing = true;
+
+        // After a second, stay visible.
+        setTimeout(() => {
+          this.visible = true;
+          this.appearing = false;
+
+          // After three seconds, disappear.
+          setTimeout(() => {
+            this.disappearing = true;
+            this.visible = false;
+
+            // After a second, reset.
+            setTimeout(() => {
+              this.disappearing = false;
+              this.email = null;
+            }, 1000)
+          }, 3000);
+        }, 1000);
       });
   }
 }
