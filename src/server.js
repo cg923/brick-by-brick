950b0d7f15304c263ff089611e1bc182b71263a6
cg923 <coreygselover@gmail.com>
2017-09-14 13:38:06 -0600

    I'm running out of ideas

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express 		= require('express');
 const app			= express();
 const bodyParser	= require('body-parser');
 
+// Cors control
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -13,14 +14,13 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
 
-let distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+app.use(express.static(__dirname + '/dist'));
 
 var routes = require('./config/routes');
 app.use(routes);
 
 app.get('*', function(req, res) {
-   res.sendFile(path.join(__dirname, 'dist/index.html'));
+   res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
