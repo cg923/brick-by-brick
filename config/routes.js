ad930f15d011d98187de491a0aac7cb27a5368c3
cg923 <coreygselover@gmail.com>
2017-09-15 15:25:58 -0600

    Updates routes file

--- config/routes.js
+++ config/routes.js
@@ -5,13 +5,43 @@ const emailsController 	= require('../controllers/emails');
 const docsController 	= require('../controllers/documents');
 const statsController 	= require('../controllers/stats');
 
+// --------- E-mails ---------- //
+// ---------------------------- //
+
+// INDEX & CREATE
 router.route('/emails')
-	.get(emailsController.getEmails);
+	.get(emailsController.getEmails)
+	.post(emailsController.postEmail);
 
+// SHOW
+router.route('/emails/:id')
+	.get(emailsController.getEmail);
+
+// UPDATE
+router.route('/emails/:id')
+	.put(emailsController.updateEmail);
+
+// DESTROY
+router.route('/emails/:id')
+	.delete(emailsController.deleteEmail);
+
+// --------- Replies ---------- //
+// ---------------------------- //
+
+// CREATE
 router.route('/replies')
-	.post(statsController.postReply);
+	.post(emailsController.postReply);
 
+// -------- Documents --------- //
+// ---------------------------- //
+// INDEX
 router.route('/documents')
 	.get(docsController.getDocuments);
+
+// ---------- Stats ----------- //
+// ---------------------------- //
+// INDEX
+router.route('/stats')
+	.get(statsController.getStats);
 
 module.exports = router;
