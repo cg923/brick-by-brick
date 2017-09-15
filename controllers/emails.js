116b251857b08f7264667ec6656d90cb61f495e6
cg923 <coreygselover@gmail.com>
2017-09-15 15:26:56 -0600

    Updates emails controller

--- controllers/emails.js
+++ controllers/emails.js
@@ -1,15 +1,108 @@
 const bodyParser 	= require('body-parser');
 const db 			= require('../models');
 
+// --------- E-mails ---------- //
+// ---------------------------- //
 // GET /emails
-// INDEX
 function getEmails(req, res) {
-	db.Email.find({}, function(err, emails) {
+	db.Email.find({}, (err, emails) => {
 		if (err) throw err;
 		res.json(emails);
 	});
 }
 
+// GET /emails/:id
+function getEmail(req, res) {
+	db.Email.findOne({_id: req.params.id}, (err, email) => {
+		if (err) throw err;
+		res.json(email);
+	});
+}
+
+// POST /emails
+function postEmail(req, res) {
+	if (typeof(req.body) === 'string') req.body = JSON.parse(req.body);
+	db.Email.create(req.body, (err, email) => {
+		if (err) throw err;
+		res.json(email);
+	});
+}
+
+// PUT /emails/:id
+function updateEmail(req, res) {
+	if (typeof(req.body) === 'string') req.body = JSON.parse(req.body);
+	db.Email.findOne({_id: req.params.id}, (err, email) => {
+		if (err) throw err;
+
+		email.to 		= req.body.to;
+		email.from 		= req.body.from;
+		email.subject 	= req.body.subject;
+		email.date 		= req.body.date;
+		email.text 		= req.body.text;
+		email.mailbox 	= req.body.mailbox;
+
+		email.save((err, email) => {
+			if (err) throw err;
+			res.json(email);
+		});
+	});
+}
+
+// DELETE /emails/:id
+function deleteEmail(req, res) {
+	db.Email.remove({_id: req.params.id}, (err, email) => {
+		if (err) throw err;
+		res.json(email);
+	});
+}
+
+// --------- Replies ---------- //
+// ---------------------------- //
+// GET /replies
+function getReplies(req, res) {
+	db.Reply.find({}, (err, replies) => {
+		if (err) throw err;
+		res.json(replies);
+	});
+}
+
+// POST /replies
+function postReply(req, res, next) {
+	if(typeof(req.body) === 'string') req.body = JSON.parse(req.body);
+
+	db.Reply.findOne({replyOption: req.body.replyOption}, (err, reply) => {
+		if (err) throw err;
+		else if(reply) {
+			reply.occurences++;
+			reply.save((err, reply) => {
+				res.json(reply);
+			});
+		}
+		else {
+			let newReply = new db.Reply({
+				to: 			req.body.to,
+			  	from: 			req.body.from,
+			  	subject: 		req.body.subject,
+			  	date: 			req.body.date,
+			  	text: 			req.body.rext,
+			  	replyOption: 	req.body.replyOption,
+			  	occurences: 	1
+			});
+
+			newReply.save((err, reply) => {
+				if(err) throw err;
+				res.json(reply);
+			});
+		}
+	});
+}
+
 module.exports = {
-	getEmails: getEmails
+	getEmails: getEmails,
+	getEmail: getEmail,
+	postEmail: postEmail,
+	updateEmail: updateEmail,
+	deleteEmail: deleteEmail,
+	getReplies: getReplies,
+	postReply: postReply
 };
