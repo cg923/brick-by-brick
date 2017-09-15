5e01aa7d720d2bf626b5f3c6319a3122458330d8
cg923 <coreygselover@gmail.com>
2017-09-15 15:26:33 -0600

    Updates stats controller

--- controllers/stats.js
+++ controllers/stats.js
@@ -1,48 +1,23 @@
 const bodyParser 	= require('body-parser');
 const db 			= require('../models');
 
-// GET /replies
-function getReplies(req, res) {
-	db.Reply.find({}, function(err, replies) {
+// GET /stats
+// INDEX
+function getStats(req, res) {
+	let responses = [];
+
+	db.Reply.find({}, function(err, emails) {
 		if (err) throw err;
-		res.json(replies);
-	});
-}
 
-// POST /replies
-function postReply(req, res, next) {
-	if(typeof(req.body) === 'string') req.body = JSON.parse(req.body);
+		//calculate stats
+		emails.forEach((element, index) => {
+			responses[index] = element.occurences;
+		});
 
-	db.Reply.findOne({replyOption: req.body.replyOption}, (err, reply) => {
-		if (err) throw err;
-		else if(reply) {
-			console.log('already exists, updating');
-			reply.occurences++;
-			reply.save((err, reply) => {
-				res.json(reply);
-			});
-		}
-		else {
-			console.log('doesnt exist, creating');
-			let newReply = new db.Reply({
-				to: 			req.body.to,
-			  	from: 			req.body.from,
-			  	subject: 		req.body.subject,
-			  	date: 			req.body.date,
-			  	text: 			req.body.rext,
-			  	replyOption: 	req.body.replyOption,
-			  	occurences: 	1
-			});
-
-			newReply.save((err, reply) => {
-				if(err) throw err;
-				res.json(reply);
-			});
-		}
+		res.json(responses);
 	});
 }
 
 module.exports = {
-	getReplies: getReplies,
-	postReply: postReply
+	getStats: getStats
 };
